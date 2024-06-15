6b214d3366ac5f22db2e607fbcd2b20c66a58dff
unknown <daudakolo16@gmail.com>
2024-06-15 17:06:44 +0100

    UI/history page

---
+++ app/(root)/search/history/page.tsx
@@ -0,0 +1,16 @@
+import HistoryCard from "@/components/historyCard";
+import { HistorMockup } from "@/utils/mockups";
+import React from "react";
+
+const Page = () => {
+  return (
+    <div className="flex flex-col gap-6 p-10 px-16">
+      <span>Search Histoy</span>
+      {HistorMockup.map((item, index) => (
+        <HistoryCard key={index} {...item} />
+      ))}
+    </div>
+  );
+};
+
+export default Page;
